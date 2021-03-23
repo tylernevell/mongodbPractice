25e13be45e19fd34d11582a3ea0a56615338958d
Tyler Nevell <tylernevell@gmail.com>
2021-03-23 15:17:37 -0600

    Connect to mongodb cluster successfully

---
+++ app.js
@@ -0,0 +1,18 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://n3vdawg:" + process.env.MONGO_PASSWORD + "@fruitcluster.3zp30.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
