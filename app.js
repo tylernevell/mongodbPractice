cd7a1d809c1008710c682730fbfa0315b364f9d5
Tyler Nevell <tylernevell@gmail.com>
2021-03-27 14:45:09 -0700

    Insert data into remote mongodb server successfully

--- app.js
+++ app.js
@@ -3,10 +3,40 @@ const uri = "mongodb+srv://n3vdawg:" + process.env.MONGO_PASSWORD + "@fruitclust
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const dbName = "fruitsDB";
+
 async function run() {
     try {
         await client.connect();
         console.log("Connected correctly to server");
+
+        const db = client.db(dbName);
+
+        // use the fruits collection
+        const col = db.collection("fruits");
+
+        // insert some documents
+        const p = await col.insertMany([
+            {
+                name : "Apple",
+                score : 8,
+                review : "Great fruit",
+            },
+            {
+                name : "Orange",
+                score : 6,
+                review : "Kind of sour",
+            },
+            {
+                name : "Banana",
+                score : 2,
+                review : "I think I'm allergic to this stuff",
+            }
+        ]);
+
+        const myDoc =  await col.findOne();
+        console.log(myDoc);
+
     } catch (err) {
         console.log(err.stack);
     }
