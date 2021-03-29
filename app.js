f3cfd825796b378628644ad992c335ebeaa56395
Tyler Nevell <tylernevell@gmail.com>
2021-03-29 11:46:04 -0700

    Change to mongoose.

--- app.js
+++ app.js
@@ -1,48 +1,157 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://n3vdawg:" + process.env.MONGO_PASSWORD + "@fruitcluster.3zp30.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = "mongodb+srv://n3vdawg:" + process.env.MONGO_PASSWORD + "@fruitcluster.3zp30.mongodb.net/fruitsDB?retryWrites=true&w=majority";
 
-const dbName = "fruitsDB";
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
-async function run() {
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
+const fruitSchema = new mongoose.Schema ({
+   name: {
+       type: String,
+       required: [true, "No name specified. Name required."]
+   },
+   rating: {
+       type: Number,
+       min: 1,
+       max: 10,
+   },
+   review: String
+});
 
-        const db = client.db(dbName);
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-        // use the fruits collection
-        const col = db.collection("fruits");
+const fruit = new Fruit({
+    name: "Apple",
+    rating: 7,
+    review: "Not bad."
+});
 
-        // insert some documents
-        const p = await col.insertMany([
-            {
-                name : "Apple",
-                score : 8,
-                review : "Great fruit",
-            },
-            {
-                name : "Orange",
-                score : 6,
-                review : "Kind of sour",
-            },
-            {
-                name : "Banana",
-                score : 2,
-                review : "I think I'm allergic to this stuff",
-            }
-        ]);
+fruit.save();
 
-        const myDoc =  await col.findOne();
-        console.log(myDoc);
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
 
-    } catch (err) {
-        console.log(err.stack);
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+   name: "John",
+   age: 37
+});
+
+//person.save();
+
+// const kiwi = new Fruit({
+//     name: "Kiwi",
+//     rating: 10,
+//     review: "Pretty freaking good."
+// });
+//
+// const banana = new Fruit({
+//     name: "banana",
+//     rating: 3,
+//     review: "Probably allergic to this."
+// });
+//
+// Fruit.insertMany([kiwi, banana], (err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Success");
+//
+//         Fruit.find((err, fruits) => {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//
+//                 mongoose.connection.close();
+//
+//                 fruits.forEach((fruit) => {
+//                     console.log(fruit.name);
+//                 });
+//             }
+//         });
+//     }
+// });
+
+Fruit.find((err, fruits) => {
+    if (err) {
+        console.log(err);
+    } else {
+
+        mongoose.connection.close();
+
+        fruits.forEach((fruit) => {
+            console.log(fruit.name);
+        });
     }
-    finally {
-        await client.close();
+});
+
+Fruit.updateOne({_id: ""}, {name: ""}, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully updated document.");
     }
-}
-run().catch(console.dir);
+});
+
+Fruit.deleteOne({_id: ""}, {name: ""}, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully deleted document.");
+    }
+});
+
+Person.deleteMany({name: "John"}, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully deleted all documents.");
+    }
+});
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//
+// const dbName = "fruitsDB";
+//
+// async function run() {
+//     try {
+//         await client.connect();
+//         console.log("Connected correctly to server");
+//
+//         const db = client.db(dbName);
+//
+//         // use the fruits collection
+//         const col = db.collection("fruits");
+//
+//         // insert some documents
+//         const p = await col.insertMany([
+//             {
+//                 name : "Apple",
+//                 score : 8,
+//                 review : "Great fruit",
+//             },
+//             {
+//                 name : "Orange",
+//                 score : 6,
+//                 review : "Kind of sour",
+//             },
+//             {
+//                 name : "Banana",
+//                 score : 2,
+//                 review : "I think I'm allergic to this stuff",
+//             }
+//         ]);
+//
+//         const myDoc =  await col.findOne();
+//         console.log(myDoc);
+//
+//     } catch (err) {
+//         console.log(err.stack);
+//     }
+//     finally {
+//         await client.close();
+//     }
+// }
+// run().catch(console.dir);
 
